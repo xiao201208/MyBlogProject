17af73af95a927ca0056703b6a27c39be4d2bf61
qitan <xiao201208@sina.com>
2018-05-20 00:24:04 +0800

    新加了后台文章展示页面

--- blog/admin.py
+++ blog/admin.py
@@ -1,7 +1,10 @@
 from django.contrib import admin
 from .models import Article, Category, Tag
 
-admin.site.register(Article)
+class ArticleAdmin(admin.ModelAdmin):
+    list_display = ['title', 'created_time', 'modified_time', 'category', 'author']
+
+admin.site.register(Article,ArticleAdmin)
 admin.site.register(Category)
 admin.site.register(Tag)
 
